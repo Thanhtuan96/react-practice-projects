8bb259a08a8a779059b0ccbe7a209c5b2fec9fe5
= <=>
2022-03-22 21:15:16 +0900

    finish front react app going to do some logical code .

---
+++ react-tailwind-css-image-gallery/src/App.js
@@ -0,0 +1,32 @@
+import React, { useState, useEffect } from 'react';
+import { ImageCard } from './components/ImageCard';
+
+function App() {
+    const [images, setImages] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
+    const [term, setTerm] = useState('');
+
+    useEffect(() => {
+        fetch(
+            `https://pixabay.com/api/?key=${process.env.REACT_APP_PIXABAY}=yellow+flowers&image_type=photo&pretty=true`
+        )
+            .then((res) => res.json())
+            .then((data) => {
+                setImages(data.hits);
+                setIsLoading(false);
+            })
+            .catch((err) => console.log(err));
+    });
+
+    return (
+        <div className='container mx-auto'>
+            <div className='grid grid-cols-3 gap-4'>
+                {images.map((img) => (
+                    <ImageCard image={img} key={img.id} />
+                ))}
+            </div>
+        </div>
+    );
+}
+
+export default App;

59ceea88e03d1814786030071bac46af4e008610
= <=>
2022-03-23 20:35:29 +0900

    finish react tailwind css image gallery..

--- react-tailwind-css-image-gallery/src/App.js
+++ react-tailwind-css-image-gallery/src/App.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { ImageCard } from './components/ImageCard';
+import { ImageSearch } from './components/ImageSearch';
 
 function App() {
     const [images, setImages] = useState([]);
@@ -8,7 +9,7 @@ function App() {
 
     useEffect(() => {
         fetch(
-            `https://pixabay.com/api/?key=${process.env.REACT_APP_PIXABAY}=yellow+flowers&image_type=photo&pretty=true`
+            `https://pixabay.com/api/?key=${process.env.REACT_APP_PIXABAY}&q=${term}&image_type=photo`
         )
             .then((res) => res.json())
             .then((data) => {
@@ -16,15 +17,31 @@ function App() {
                 setIsLoading(false);
             })
             .catch((err) => console.log(err));
-    });
+    }, [term]);
 
     return (
         <div className='container mx-auto'>
-            <div className='grid grid-cols-3 gap-4'>
-                {images.map((img) => (
-                    <ImageCard image={img} key={img.id} />
-                ))}
-            </div>
+            <ImageSearch searchText={(text) => setTerm(text)} />
+            {!isLoading && images.length === 0 && (
+                <h1 className='text-5xl text-center mx-auto mt-32'>
+                    No Images Found
+                </h1>
+            )}
+            {isLoading ? (
+                <h1 className='text-6xl text-center mx-auto'>Loading...</h1>
+            ) : (
+                <>
+                    <div className='grid grid-cols-3 gap-4'>
+                        {images ? (
+                            images.map((img) => (
+                                <ImageCard image={img} key={img.id} />
+                            ))
+                        ) : (
+                            <h1>It seemed nothings here</h1>
+                        )}
+                    </div>
+                </>
+            )}
         </div>
     );
 }
--- react-tailwind-css-image-gallery/src/components/ImageCard.jsx
+++ react-tailwind-css-image-gallery/src/components/ImageCard.jsx
@@ -26,9 +26,9 @@ export const ImageCard = ({ image }) => {
                 </ul>
             </div>
             <div className='px-6 py-4'>
-                {tags.map((tag) => (
+                {tags.map((tag, index) => (
                     <span
-                        key={image.id}
+                        key={index}
                         className='inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2'
                     >
                         #{tag}
